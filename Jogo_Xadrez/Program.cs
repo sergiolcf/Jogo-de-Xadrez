6cecde16b67f9b633e78ec03ebfe2a5799646bad
Sergio Ferreira <sergiolc.ferreira@hotmail.com>
2020-05-30 15:52:50 -0300

    Começando a colocando peças no Tabuleiro

--- Jogo_Xadrez/Program.cs
+++ Jogo_Xadrez/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using tabuleiro;
-
+using xadrez;
 namespace Jogo_Xadrez
 {
     class Program
@@ -8,8 +8,11 @@ namespace Jogo_Xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.colcoarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colcoarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colcoarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
             Tela.imprimirTabuleiro(tab);
-            Console.WriteLine(tab);
+            Console.ReadLine();
 
         }
     }
--- Jogo_Xadrez/Tabuleiro/Tabuleiro.cs
+++ Jogo_Xadrez/Tabuleiro/Tabuleiro.cs
@@ -17,5 +17,10 @@
         {
             return pecas[linha, coluna];
         }
+        public void colcoarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
