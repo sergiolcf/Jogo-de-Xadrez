caa5c4501fb0d2dd00052a7a74576e606e31255c
Sergio Ferreira <sergiolc.ferreira@hotmail.com>
2020-05-30 14:42:56 -0300

    Criando classe posição

--- Jogo_Xadrez/Program.cs
+++ Jogo_Xadrez/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Jogo_Xadrez
 {
@@ -10,7 +7,9 @@ namespace Jogo_Xadrez
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3, 4);
 
+            Console.WriteLine($"Posição {p}");
         }
     }
 }
